a368b58dc8ed09c0a0c9f958bc9927636a597468
hani <brightgowon.hye@gmail.com>
2024-05-28 01:09:07 -0300

    feat: callback event

--- telegram/event.go
+++ telegram/event.go
@@ -3,8 +3,40 @@ package telegram
 import "encoding/json"
 
 type Event struct {
-	UpdateID int     `json:"update_id"`
-	Message  Message `json:"message"`
+	UpdateID      int           `json:"update_id"`
+	Message       Message       `json:"message"`
+	CallbackQuery CallbackQuery `json:"callback_query"`
+}
+
+type CallbackQuery struct {
+	ID           string  `json:"id"`
+	From         From    `json:"from"`
+	Message      Message `json:"message"`
+	ChatInstance string  `json:"chat_instance"`
+	Data         string  `json:"data"`
+}
+
+type UserClass struct {
+	ID           int64  `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Username     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+
+type CaptionEntity struct {
+	Offset int64  `json:"offset"`
+	Length int64  `json:"length"`
+	Type   string `json:"type"`
+	User   From   `json:"user,omitempty"`
+}
+type Photo struct {
+	FileID       string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
+	FileSize     int64  `json:"file_size"`
+	Width        int64  `json:"width"`
+	Height       int64  `json:"height"`
 }
 
 func ParseEvent(data []byte) (*Event, error) {
--- telegram/message.go
+++ telegram/message.go
@@ -1,11 +1,15 @@
 package telegram
 
 type Message struct {
-	MessageID int    `json:"message_id"`
-	From      From   `json:"from"`
-	Chat      Chat   `json:"chat"`
-	Date      int    `json:"date"`
-	Text      string `json:"text"`
+	MessageID       int64           `json:"message_id"`
+	From            From            `json:"from"`
+	Chat            Chat            `json:"chat"`
+	Date            int64           `json:"date"`
+	Text            string          `json:"text"`
+	Photo           []Photo         `json:"photo"`
+	Caption         string          `json:"caption"`
+	CaptionEntities []CaptionEntity `json:"caption_entities"`
+	ReplyMarkup     ReplyMarkup     `json:"reply_markup"`
 }
 
 type From struct {
