7e756b7d8d4d995aee078ef4fd6b0b92ccb302ff
hani <brightgowon.hye@gmail.com>
2024-05-27 22:29:45 -0300

    feat: new inlinekeyboard

--- telegram/request_message.go
+++ telegram/request_message.go
@@ -4,6 +4,8 @@ type RequestMessage struct {
 	ChatID              int             `json:"chat_id"`
 	Text                string          `json:"text"`
 	Entities            []MessageEntity `json:"entities"`
+	ReplyParametes      ReplyParametes  `json:"reply_parametes"`
+	ReplyMarkup         InlineKeyboard  `json:"reply_markup"`
 	ParseMode           ParseMode       `json:"parse_mode"`
 	DisableNotification bool            `json:"disable_notification"`
 	ProtectContent      bool            `json:"protect_content"`
@@ -20,4 +22,18 @@ type MessageEntity struct {
 	Type   string `json:"type"`
 	Offset int    `json:"offset"`
 	Length int    `json:"length"`
+}
+
+type InlineKeyboard struct {
+	Text                         string `json:"text"`
+	URL                          string `json:"url"`
+	CallbackData                 string `json:"callback_data"`
+	SwitchInlineQuery            string `json:"switch_inline_query"`
+	SwitchInlineQueryCurrentChat string `json:"switch_inline_query_current_chat"`
+	Pay                          bool   `json:"pay"  bson:"pay"`
+}
+
+type ReplyParametes struct {
+	MessageID int `json:"message_id"`
+	ChatID    int `json:"chat_id"`
 }
