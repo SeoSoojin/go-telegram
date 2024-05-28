b0e97405f82dd8e28f197c848aea75c9c27b7f2e
hani <brightgowon.hye@gmail.com>
2024-05-27 23:13:05 -0300

    feat: sendPhoto req

--- telegram/client.go
+++ telegram/client.go
@@ -11,6 +11,7 @@ import (
 
 type Client interface {
 	SendMessage(req RequestMessage) (Message, error)
+	SendPhoto(req RequestPhoto) (Message, error)
 }
 
 type client struct {
@@ -43,6 +44,47 @@ func (c *client) SendMessage(message RequestMessage) (Message, error) {
 	u := c.baseURL.ResolveReference(&url.URL{Path: "sendMessage"})
 
 	b, err := json.Marshal(message)
+	if err != nil {
+		return *resp, err
+	}
+
+	httpReq, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewBuffer(b))
+	if err != nil {
+		return *resp, err
+	}
+
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	httpResp, err := c.httpClient.Do(httpReq)
+	if err != nil {
+		return *resp, err
+	}
+
+	defer httpResp.Body.Close()
+
+	if httpResp.StatusCode != http.StatusOK {
+		return *resp, fmt.Errorf("unexpected status code: %d", httpResp.StatusCode)
+	}
+
+	b, err = io.ReadAll(httpResp.Body)
+	if err != nil {
+		return *resp, err
+	}
+
+	err = json.Unmarshal(b, resp)
+	if err != nil {
+		return *resp, err
+	}
+
+	return *resp, nil
+}
+
+func (c *client) SendPhoto(photo RequestPhoto) (Message, error) {
+	resp := new(Message)
+
+	u := c.baseURL.ResolveReference(&url.URL{Path: "sendPhoto"})
+
+	b, err := json.Marshal(photo)
 	if err != nil {
 		return *resp, err
 	}
---
+++ telegram/request_image.go
@@ -0,0 +1,10 @@
+package telegram
+
+type RequestPhoto struct {
+	ChatID         int            `json:"chat_id"`
+	Photo          string         `json:"photo"`
+	Caption        string         `json:"caption"`
+	ReplyParametes ReplyParametes `json:"reply_parametes"`
+	ReplyMarkup    ReplyMarkup    `json:"reply_markup"`
+	ParseMode      ParseMode      `json:"parse_mode"`
+}
