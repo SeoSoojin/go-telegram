16c700eb373f96631984d8b364dbecfd1e26f2d2
hani <brightgowon.hye@gmail.com>
2024-05-29 12:56:03 -0300

    feat: some emojis

---
+++ emojis/emojis.go
@@ -0,0 +1,18 @@
+package emojis
+
+type Emoji struct {
+	Emoji   string
+	Code    string
+	Unicode string
+}
+
+var (
+	Smile     = Emoji{Emoji: "😊", Code: ":smile:", Unicode: ""}
+	Heart     = Emoji{Emoji: "❤️", Code: ":heart:", Unicode: ""}
+	Candy     = Emoji{Emoji: "🍬", Code: ":candy:", Unicode: ""}
+	Lollipop  = Emoji{Emoji: "🍭", Code: ":lollipop:", Unicode: ""}
+	Cupcake   = Emoji{Emoji: "🧁", Code: ":cupcake:", Unicode: ""}
+	Bingsoo   = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
+	ShavedIce = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
+	FireHeart = Emoji{Emoji: "❤️‍🔥", Code: ":fire_heart:", Unicode: ""}
+)
