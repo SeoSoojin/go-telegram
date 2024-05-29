43c95843e492e31b7a286afcd0dab2a80a747abe
hani <brightgowon.hye@gmail.com>
2024-05-29 12:57:31 -0300

    feat: some emojis

--- emojis/emojis.go
+++ emojis/emojis.go
@@ -7,12 +7,15 @@ type Emoji struct {
 }
 
 var (
-	Smile     = Emoji{Emoji: "😊", Code: ":smile:", Unicode: ""}
-	Heart     = Emoji{Emoji: "❤️", Code: ":heart:", Unicode: ""}
-	Candy     = Emoji{Emoji: "🍬", Code: ":candy:", Unicode: ""}
-	Lollipop  = Emoji{Emoji: "🍭", Code: ":lollipop:", Unicode: ""}
-	Cupcake   = Emoji{Emoji: "🧁", Code: ":cupcake:", Unicode: ""}
-	Bingsoo   = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
-	ShavedIce = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
-	FireHeart = Emoji{Emoji: "❤️‍🔥", Code: ":fire_heart:", Unicode: ""}
+	Smile         = Emoji{Emoji: "😊", Code: ":smile:", Unicode: ""}
+	Heart         = Emoji{Emoji: "❤️", Code: ":heart:", Unicode: ""}
+	Candy         = Emoji{Emoji: "🍬", Code: ":candy:", Unicode: ""}
+	Lollipop      = Emoji{Emoji: "🍭", Code: ":lollipop:", Unicode: ""}
+	Cupcake       = Emoji{Emoji: "🧁", Code: ":cupcake:", Unicode: ""}
+	Bingsoo       = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
+	ShavedIce     = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
+	FireHeart     = Emoji{Emoji: "❤️‍🔥", Code: ":fire_heart:", Unicode: ""}
+	Duck          = Emoji{Emoji: "🦆", Code: ":duck:", Unicode: ""}
+	Chick         = Emoji{Emoji: "🐥", Code: ":chick:", Unicode: ""}
+	HatchingChick = Emoji{Emoji: "🐣", Code: ":hatching_chick:", Unicode: ""}
 )
