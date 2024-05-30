133279242013e3c02c93c303a03e645649007ce4
hani <brightgowon.hye@gmail.com>
2024-05-30 16:34:29 -0300

    feat: some unicodes

--- emojis/emojis.go
+++ emojis/emojis.go
@@ -9,11 +9,11 @@ type Emoji struct {
 var (
 	Smile         = Emoji{Emoji: "😊", Code: ":smile:", Unicode: ""}
 	Heart         = Emoji{Emoji: "❤️", Code: ":heart:", Unicode: ""}
-	Candy         = Emoji{Emoji: "🍬", Code: ":candy:", Unicode: ""}
-	Lollipop      = Emoji{Emoji: "🍭", Code: ":lollipop:", Unicode: ""}
-	Cupcake       = Emoji{Emoji: "🧁", Code: ":cupcake:", Unicode: ""}
-	Bingsoo       = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
-	ShavedIce     = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: ""}
+	Candy         = Emoji{Emoji: "🍬", Code: ":candy:", Unicode: "1F367"}
+	Lollipop      = Emoji{Emoji: "🍭", Code: ":lollipop:", Unicode: "1F36D"}
+	Cupcake       = Emoji{Emoji: "🧁", Code: ":cupcake:", Unicode: "1F9C1"}
+	Bingsoo       = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: "1F367"}
+	ShavedIce     = Emoji{Emoji: "🍧", Code: ":shaved_ice:", Unicode: "1F367"}
 	FireHeart     = Emoji{Emoji: "❤️‍🔥", Code: ":fire_heart:", Unicode: ""}
 	Duck          = Emoji{Emoji: "🦆", Code: ":duck:", Unicode: ""}
 	Chick         = Emoji{Emoji: "🐥", Code: ":chick:", Unicode: ""}
